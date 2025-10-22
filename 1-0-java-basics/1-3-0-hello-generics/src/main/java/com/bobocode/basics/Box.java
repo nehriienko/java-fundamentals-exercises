29c78be385086e7c60f5c3188ef26210861342f8
nehriienko <sasha.negrienko2007@gmail.com>
2025-10-23 00:03:05 +0300

    Виконані завдання з java-fundamentals-exercises

--- 1-0-java-basics/1-3-0-hello-generics/src/main/java/com/bobocode/basics/Box.java
+++ 1-0-java-basics/1-3-0-hello-generics/src/main/java/com/bobocode/basics/Box.java
@@ -1,24 +1,18 @@
 package com.bobocode.basics;
 
-/**
- * {@link Box} is a container class that can store a value of any given type. Using Object as a field type
- * is flexible, because we can store anything we want there. But it is not safe, because it requires runtime casting
- * and there is no guarantee that we know the type of the stored value.
- * <p>
- * todo: refactor this class so it uses generic type "T" and run {@link com.bobocode.basics.BoxTest} to verify it
- */
-public class Box {
-    private Object value;
 
-    public Box(Object value) {
+public class Box<T> {
+    private T value;
+
+    public Box(T value) {
         this.value = value;
     }
 
-    public Object getValue() {
+    public T getValue() {
         return value;
     }
 
-    public void setValue(Object value) {
+    public void setValue(T value) {
         this.value = value;
     }
 }
